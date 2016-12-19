1cb24c7bad8aa50b6dd5d9beba107be3e544e25b
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-12-19 10:54:17 -0800

    using md5sum from coreutils for md5 calculation

--- zsh-cwd-history.zsh
+++ zsh-cwd-history.zsh
@@ -40,7 +40,7 @@ ZSH_CWD_HISTORY_SHOW_CHPWD_MSG=false
 # Echo the name of the history file to use for current working dir
 # Uses the md5 hash of the absolute path
 _zsh_cwd_history_histfile_for_cwd() {
-	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | md5 -q)"
+	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | md5sum | cut -d" " -f1)"
 }
 
 # Switch to a new history file, writing to the old one
